a25f4e616b961ad9caff80fac87209f9fca14238
shlifedev <shlifedev@gmail.com>
2023-02-06 14:06:09 +0900

    .

--- Assets/UnityPen/Scripts/Backend/JavascriptBehaviour.cs
+++ Assets/UnityPen/Scripts/Backend/JavascriptBehaviour.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using Jint;
 using Jint.Native;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityPen;
 
@@ -30,19 +31,24 @@ namespace UnityPen
             {
                 cfg.AllowClr(typeof(Vector3).Assembly);
                 cfg.AllowClr(typeof(GameObject).Assembly); 
-            });     
+            });      
             // 값의 설정은 미리한다.
             engine.SetValue(nameof(Debug.Log), new Action<object>(Debug.Log)); 
             engine.SetValue(nameof(Mathf), typeof(Mathf));
             engine.SetValue(nameof(Vector3), typeof(Vector3));
             engine.SetValue(nameof(GameObject), typeof(GameObject));  
+            engine.SetValue(nameof(Quaternion), typeof(Quaternion));  
+            engine.SetValue(nameof(Transform), typeof(Transform));  
             engine.SetValue(nameof(Time), typeof(Time));  
             engine.SetValue("self", (this as object));  
             engine.Execute(code); 
             // 가져오는것은 Execute 후.
             this.JsUpdate = engine.GetValue("onUpdate");
             this.JSStart = engine.GetValue("onStart");
-             
+
+
+            this.transform.rotation = Quaternion.Euler(1,1,1);
+            
         }
         void Awake()
         {
